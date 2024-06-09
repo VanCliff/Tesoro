93e5293493c678c1911a0784bc58889f7041197f
Gabriel <vancliff@outlook.fr>
2024-06-09 16:39:19 +0200

    Init

---
+++ src/main/java/pe/gobierno/tesoro/TesoroApplication.java
@@ -0,0 +1,13 @@
+package pe.gobierno.tesoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gobierno/tesoro/TesoroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gobierno.tesoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
